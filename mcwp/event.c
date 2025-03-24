b2cf9edea9ca3f2f381a3432c7474a1e32de175f
foreverzmy <921255465@qq.com>
2025-03-24 11:55:42 +0800

    feat: support CustomEvent constructor

--- mcwp/event.c
+++ mcwp/event.c
@@ -8,34 +8,35 @@
 #include "event.h"
 
 JSClassID js_event_class_id = 0;
+JSClassID js_custom_event_class_id = 0;
 
 // Event构造函数
-static JSValue js_event_constructor(JSContext *ctx, JSValueConst new_target, int argc, JSValueConst *argv) {
+static JSValue js_event_constructor(JSContext *ctx, JSValueConst new_target, int argc, JSValueConst *argv, int magic) {
   JSValue obj = JS_UNDEFINED;
   Event *event = NULL;
   const char *type = "";
   bool bubbles = false;
   bool cancelable = false;
   bool composed = false;
+  JSValue detail = JS_UNDEFINED;
 
   if (JS_IsUndefined(new_target))
     return JS_ThrowTypeError(ctx, "Constructor Event requires 'new'");
 
-  // 创建JS对象
-  obj = JS_NewObjectClass(ctx, js_event_class_id);
-  if (JS_IsException(obj))
-    goto fail;
-
   if (argc < 1)
     return JS_ThrowTypeError(ctx, "1 argument required, but only 0 present");
 
-  // 解析参数
+  // 创建JS对象
+  obj = JS_NewObjectClass(ctx, magic == 0 ? js_event_class_id : js_custom_event_class_id);
+  if (JS_IsException(obj))
+    goto fail;
 
+  // 解析参数
   type = JS_ToCString(ctx, argv[0]);
   if (!type)
     goto fail;
 
-  if (argc > 1 && !JS_IsUndefined(argv[1])) {
+  if (argc > 1 && JS_IsObject(argv[1])) {
     JSValue bubbles_val = JS_GetPropertyStr(ctx, argv[1], "bubbles");
     if (!JS_IsException(bubbles_val))
       bubbles = JS_ToBool(ctx, bubbles_val);
@@ -50,6 +51,11 @@ static JSValue js_event_constructor(JSContext *ctx, JSValueConst new_target, int
     if (!JS_IsException(composed_val))
       composed = JS_ToBool(ctx, composed_val);
     JS_FreeValue(ctx, composed_val);
+
+    JSValue detail = JS_GetPropertyStr(ctx, argv[1], "detail");
+    if (!JS_IsException(detail))
+      detail = JS_DupValue(ctx, detail);
+    JS_FreeValue(ctx, detail);
   }
 
   // 创建Event对象
@@ -66,7 +72,7 @@ static JSValue js_event_constructor(JSContext *ctx, JSValueConst new_target, int
   event->target = JS_UNDEFINED;
   event->currentTarget = JS_UNDEFINED;
   event->relatedTarget = JS_UNDEFINED;
-  event->detail = JS_UNDEFINED;
+  event->detail = detail;
   event->isTrusted = false;
 
   JS_SetOpaque(obj, event);
@@ -125,35 +131,46 @@ static JSClassDef js_event_class_def = {
     .finalizer = js_event_finalizer,
 };
 
+static JSClassDef js_custom_event_class_def = {
+    "CustomEvent",
+    .finalizer = js_event_finalizer,
+};
+
 static JSCFunctionListEntry js_event_proto_funcs[] = {};
 
 void js_init_event(JSContext *ctx) {
   JSValue event_proto, event_class;
+  JSValue custom_event_proto, custom_event_class;
   // JSValue event_target_proto, event_target_class;
-  // JSValue custom_event_proto, custom_event_class;
-  // , event_target_class_id, custom_event_class_id
 
   // 创建Event类
   JS_NewClassID(&js_event_class_id);
-
   JS_NewClass(JS_GetRuntime(ctx), js_event_class_id, &js_event_class_def);
-
   // 创建Event原型
   event_proto = JS_NewObject(ctx);
-
   // 为原型对象添加方法
   JS_SetPropertyFunctionList(ctx, event_proto, js_event_proto_funcs, countof(js_event_proto_funcs));
-
   // Create constructor，创建构造函数
-  event_class = JS_NewCFunction2(ctx, js_event_constructor, "Event", 1, JS_CFUNC_constructor, 0);
-
+  event_class = JS_NewCFunctionMagic(ctx, js_event_constructor, "Event", 1, JS_CFUNC_constructor_magic, 0);
+  // 设置Event原型和类
   JS_SetConstructor(ctx, event_class, event_proto);
   JS_SetClassProto(ctx, js_event_class_id, event_proto);
+
+  // 创建CustomEvent类
+  JS_NewClassID(&js_custom_event_class_id);
+  JS_NewClass(JS_GetRuntime(ctx), js_custom_event_class_id, &js_custom_event_class_def);
+  // 创建CustomEvent原型
+  custom_event_proto = JS_NewObjectProto(ctx, event_proto); // 继承自Event.prototype
+  custom_event_class = JS_NewCFunctionMagic(ctx, js_event_constructor, "CustomEvent", 1, JS_CFUNC_constructor_magic, 1);
+  // 设置CustomEvent原型和类
+  JS_SetConstructor(ctx, custom_event_class, custom_event_proto);
+  JS_SetClassProto(ctx, js_custom_event_class_id, custom_event_proto);
 
   JSValue global_obj = JS_GetGlobalObject(ctx);
 
   // Set the class as a global property
   JS_SetPropertyStr(ctx, global_obj, "Event", event_class);
+  JS_SetPropertyStr(ctx, global_obj, "CustomEvent", custom_event_class);
 
   JS_FreeValue(ctx, global_obj);
 }
--- tests/event.js
+++ tests/event.js
@@ -4,7 +4,9 @@ try {
   console.log(e1);
   console.log('===========end===========');
 } catch (error) {
+  console.log('----------start----------');
   console.error(error);
+  console.log('===========end===========');
 }
 const e1 = new Event("tt");
 console.log('----------start----------');
@@ -12,3 +14,11 @@ console.log(e1);
 console.log('===========end===========');
 console.info(e1 instanceof Event);
 
+const ce1 = new CustomEvent("tt");
+console.log('----------start----------');
+console.log(ce1);
+console.log('===========end===========');
+console.info(ce1 instanceof CustomEvent);
+console.info(ce1 instanceof Event);
+
+console.info("--------end---------");
